9416f2157705a66f78e9d80853fdea496e88080c
Jake Brisk <jakebrisk@gmail.com>
2017-01-21 15:02:10 -0500

    update build script

--- make.bash
+++ make.bash
@@ -1,3 +1,9 @@
-GOOGLE_COMPILER='closure-compiler.jar'
 
-java -jar 'closure-compiler.jar' --js scripts/content/main.js --js scripts/utils/CORS.js --js scripts/utils/utils.js --js_output_file scripts/content_compiled/play_music.js
+## script to build the Lyrical extension ##
+
+COMPILER='closure-compiler.jar'
+
+PLAY_MUSIC_FILES='--js scripts/content/pm_ctrl.js --js scripts/utils/lyrics.js --js scripts/utils/utils.js --js scripts/utils/CORS.js'
+
+# build the Google Play Music content script
+java -jar $COMPILER $PLAY_MUSIC_FILES --js_output_file scripts/content_compiled/play_music.js
