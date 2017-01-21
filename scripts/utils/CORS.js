2caee72dbd7e05ce10a5a5d9ce502a23b4d6e55f
Jake Brisk <jakebrisk@gmail.com>
2017-01-21 17:07:40 -0500

    more code cleanup

--- scripts/utils/CORS.js
+++ scripts/utils/CORS.js
@@ -1,37 +1,20 @@
 // utility functions to deal with CORS requests
 
 const $CORS = {
-	// Create the XHR object.
+	// Create the XHR object. (https://www.html5rocks.com/en/tutorials/cors/)
 	createCORSRequest: function(method, url) {
-	  var xhr = new XMLHttpRequest();
-	  if ("withCredentials" in xhr) {
-	    // XHR for Chrome/Firefox/Opera/Safari.
-	    xhr.open(method, url, true);
-	  } else {
-	    // CORS not supported.
-	    xhr = null;
-	  }
-	  return xhr;
+		let xhr = new XMLHttpRequest();
+		if ("withCredentials" in xhr) {
+			xhr.open(method, url, true);
+		} else {
+			// CORS not supported.
+			xhr = null;
+		}
+		return xhr;
 	},
 
 	extract_lyrics: function(text){
-		return text.match('<div class="lyrics" (.*?)>(.*?)</div>');
-	},
-
-	// Make the actual CORS request.
-	makeCorsRequest: function(url) {
-
-	  var xhr = $CORS.createCORSRequest('GET', url);
-	  if (!xhr) {
-	    alert('CORS not supported');
-	    return;
-	  }
-
-	  // Response handlers.
-	  xhr.onload = function() {
-	    var text = xhr.responseText;
-
-	    // Find lyrics in the returned document
+		// Find lyrics in the returned document
 	   	let parser = new DOMParser();
 	   	let doc = parser.parseFromString(text, 'text/html');
 	    let actual_lyrics = $(doc).find(".lyrics").text();
@@ -41,23 +24,41 @@ const $CORS = {
 	    actual_lyrics.forEach(function(element, index, arr){
 	    	arr[index] = "<p>" + arr[index] + "</p>";
 	    });
-	    
+
 	    // Finally, show the lyrics
 	    $("#lyrics").append(actual_lyrics);
 	    // Credit website
 	    $("#lyrics").prepend(`<p style="text-align:right">Lyrics from <a href="${url}" target="_blank">Genius</a></p>`);
 
-	    // Clean up
-	    xhr = null;
 	    parser = null
 	    doc = null;
-	  };
+	},
 
-	  xhr.onerror = function() {
-	    console.err("xhr request failed");
-	  };
+	// Make the actual CORS request.
+	makeCorsRequest: function(url) {
 
-	  xhr.send();
+		let xhr = $CORS.createCORSRequest('GET', url);
+		if (!xhr) {
+			console.log('CORS not supported');
+			return;
+		}
+
+		// Response handlers.
+		xhr.onload = function() {
+			let text = xhr.responseText;
+
+			$CORS.extract_lyrics(text);
+
+			// Clean up
+			xhr = null;
+
+		};
+
+		xhr.onerror = function() {
+			console.err("xhr request failed");
+		};
+
+		xhr.send();
 	}
 }
 
