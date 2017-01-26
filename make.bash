4e9c4b1604fbb60a482ccf0ce181af0189b9cda3
Jake Brisk <jakebrisk@gmail.com>
2017-01-26 16:30:17 -0500

    change file location

--- make.bash
+++ make.bash
@@ -9,3 +9,4 @@ YOUTUBE_FILES='--js scripts/content/yt_ctrl.js --js scripts/utils/lyrics.js --js
 # build the content scripts
 java -jar $COMPILER $PLAY_MUSIC_FILES --js_output_file scripts/content_compiled/play_music.js
 java -jar $COMPILER $YOUTUBE_FILES --js_output_file scripts/content_compiled/youtube.js
+cp scripts/utils/resize_drag.js scripts/content_compiled/
