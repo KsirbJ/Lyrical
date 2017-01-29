79a5ea4471e008751d99b105f709296075cba212
Jake Brisk <jakebrisk@gmail.com>
2017-01-28 23:28:10 -0500

    ui updates

--- scripts/utils/panel.js
+++ scripts/utils/panel.js
@@ -6,6 +6,7 @@ const $panel = {
 	append_styles: function(){
 		$("head").append(`
 			<style type="text/css">
+				@import url('https://fonts.googleapis.com/css?family=Kalam:400,700');
 				html, body {
 					cursor: default;
 				}
@@ -18,7 +19,6 @@ const $panel = {
 				#lyrics {
 					background: #fff;
 					padding: 1em 3em 1em 2em;
-					margin: 0;
 					overflow-y: scroll;
 					-webkit-font-smoothing: subpixel-antialiased;
 					transform: translateZ(0) scale(1.0, 1.0);
@@ -26,6 +26,10 @@ const $panel = {
 					filter: blur(0);
 					transform-origin: 50%  53%;
 					perspective: 1000px;
+					border-top: 1px solid #FFCF90;
+					border-bottom: 1px solid #FFCF90;
+					border-left: 1px solid #FFCF90;
+					margin: 0 0 1em .7em;
 				}
 				#lyrics p{
 					-webkit-font-smoothing: subpixel-antialiased;
@@ -39,7 +43,6 @@ const $panel = {
 					position: absolute;
 					top: 0;
 					right: 0;
-					border: 1px solid #000;
 					background: #fff;
 					z-index: 199999000000;
 					overflow: auto;
@@ -72,8 +75,9 @@ const $panel = {
 				}
 				#words {
 					background: #fff;
-					margin-top: 1em;
-					padding-top: 1em;
+					margin: 1em 0 0 0;
+					padding-top: 2.5em;
+					color: #000;
 				}
 				#err_msg {
 					padding-top: 30%;
@@ -89,20 +93,79 @@ const $panel = {
     				right: 0;
 				}
 				#credits {
-					position: fixed;
-					top: 10px;
-					right: 25px;
+					position: absolute;
+					top: 40px;
+					right: 10px;
+					font-size: .9em;
 				}
 				#credits a {
 					color: #0000ff;
 				}
-				#show_hide_lyrics {
-					border: 1px solid #000;
-					padding: 1em;
-					background: #fff;
-					text-decoration: none;
+				#lyrical_icon {
+					position: absolute;
+					top: 6px;
+					right: 6px;
+					z-index: 191919191919;
+					height: 23px;
 				}
-
+				#lyrics::-webkit-scrollbar {
+				  width: 11px;
+				  height: 10px;
+				}
+				#lyrics::-webkit-scrollbar-button {
+				  width: 0px;
+				  height: 0px;
+				}
+				#lyrics::-webkit-scrollbar-thumb {
+				  background: #ffcf90;
+				  border: 1px solid #fff;
+				  border-radius: 10px;
+				}
+				#lyrics::-webkit-scrollbar-thumb:hover {
+				  background: #ffcf90;
+				}
+				#lyrics::-webkit-scrollbar-thumb:active {
+				  background: #ffcf90;
+				}
+				#lyrics::-webkit-scrollbar-track {
+				  background: #ffffff;
+				  border: 1px solid #ffcf90;
+				}
+				#lyrics::-webkit-scrollbar-track:hover {
+				  background: #ffffff;
+				}
+				#lyrics::-webkit-scrollbar-track:active {
+				  background: #ffffff;
+				}
+				#lyrics::-webkit-scrollbar-corner {
+				  background: transparent;
+				}
+				#lyrical_title {
+					font-family: 'Kalam', cursive;
+					font-weight: 700;
+					color: #fff;
+					font-size: 2em;
+					position: absolute;
+					top: -2px;
+					right: 40px;
+					margin: 0;
+				}
+				#show_hide_lyrics {
+					display: block;
+					padding: .8em;
+					overflow: hidden;
+					border-radius: 2px;
+					box-shadow: 0 1px 4px rgba(0, 0, 0, .35);  
+					background-color: #ff9102;
+					color: #fff;		  
+					text-decoration: none;
+					transition: background-color .3s;
+				}
+				#show_hide_lyrics:hover {
+					background-color: #ee792c;
+					text-decoration: none;
+					transition: background-color .3s;
+				}
 			</style>
 		`);
 	},
@@ -110,16 +173,29 @@ const $panel = {
 	// Add the panel to the view
 	prepend_panel: function(toWhat){
 		$(toWhat).prepend(`<div id="lyrics">
-			<div class="btn"><a href="#" class="pop_out_btn" id="pop-in-out" data-state="is_in"></a></div>
+			<div class="btn">
+				<a href="#" class="pop_out_btn" id="pop-in-out" data-state="is_in"></a>
+				<h2 id="lyrical_title">Lyrical</h2>
+				<img src="" id="lyrical_icon" />
+			</div>
 			<div id="words"><div id="err_msg">Play a song to see lyrics</div></div>
 			</div>`);
-	},
+
+			let img = chrome.extension.getURL("img/icon-128.png");
+			$("#lyrical_icon").attr('src', img);
+	},	
 
 	append_panel: function(toWhat){
 		$(toWhat).append(`<div id="lyrics">
-			<div class="btn"><a href="#" class="pop_out_btn" id="pop-in-out" data-state="is_in"></a></div>
+			<div class="btn">
+				<a href="#" class="pop_out_btn" id="pop-in-out" data-state="is_in"></a>
+				<h2 id="lyrical_title">Lyrical</h2>
+				<img src="" id="lyrical_icon" />
+			</div>
 			<div id="words"><div id="err_msg">Play a song to see lyrics</div></div>
 			</div>`);
+		let img = chrome.extension.getURL("img/icon-128.png");
+		$("#lyrical_icon").attr('src', img);
 	},
 
 	// Show or hide the panel
