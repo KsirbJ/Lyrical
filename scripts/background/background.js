05ee429ce433e03b51ed94a61f19f08038dd38f8
ksirbj <ksirbj@users.noreply.github.com>
2018-03-06 14:48:58 -0500

    add clear cache option

--- scripts/background/background.js
+++ scripts/background/background.js
@@ -25,6 +25,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 			const responder = new ResponseHandler(sender);
 			Cache.get_item(request.id, responder.sendResponse.bind(responder));
 			break;
+		case 'clear-cache':
+			Cache.clear();
 		default:
 			break;
 	}
--- ui/options.js
+++ ui/options.js
@@ -1,15 +1,34 @@
 
 
 $(function(){
+
+	const OPTS = {
+		'run_on_gp': true, 
+		'run_on_yt': true, 
+		'run_on_sp': true, 
+		'autoscroll': false, 
+		'run_all': false, 
+		'yt_detect_mode': true, 
+		'yt_mem': true, 
+		'pm_mem': true, 
+		'sp_mem': true,
+		'yt_as': false, 
+		'pm_as': false, 
+		'sp_as': false
+	};
+
+	const showResMsg = (msg) => {
+		$("#response_msg").text(msg);
+		setTimeout(() => {
+			$("#response_msg").text("");
+		}, 2000);
+	}
+
 	// Restore saved settings on page load
 	chrome.storage.sync.clear();
 	function pull_options(){
-		chrome.storage.local.get({'run_on_gp': true, 'run_on_yt': true, 'run_on_sp': true, 'autoscroll': false, 
-			'run_all': false, 'yt_detect_mode': true, 'yt_mem': true, 'pm_mem': true, 'sp_mem': true,
-			'yt_as': false, 'pm_as': false, 'sp_as': false}, 
-			function(response){
-			//console.log(response);
-			for(opt in response){
+		chrome.storage.local.get(OPTS, (response) => {
+			for(const opt in response){
 				if(response[opt]){
 					$(`#${opt}`).prop("checked", true);
 				}else{
@@ -21,42 +40,25 @@ $(function(){
 	
 	// Save options on click
 	function update_options(e){
-		let run_on_gp = $("#run_on_gp").prop('checked');
-		let run_on_yt = $("#run_on_yt").prop('checked');
-		let run_on_sp = $("#run_on_sp").prop('checked');
-		let autoscroll = $("#autoscroll").prop('checked');
-		let run_all = $('#run_all').prop('checked');
-		let yt_detect_mode = $("#yt_detect_mode").prop('checked');
-		let yt_mem = $("#yt_mem").prop('checked');
-		let pm_mem = $("#pm_mem").prop('checked');
-		let sp_mem = $("#sp_mem").prop('checked');
-		let yt_as = $("#yt_as").prop('checked');
-		let pm_as = $("#pm_as").prop('checked');
-		let sp_as = $("#sp_as").prop('checked');
+		for(const opt in OPTS){
+			OPTS[opt] = $(`#${opt}`).prop('checked');
+		}
 
-		chrome.storage.local.set({'run_on_gp': run_on_gp, 'run_on_yt': run_on_yt, 'run_on_sp': run_on_sp, 
-			'autoscroll': autoscroll, 'run_all': run_all, 'yt_detect_mode': yt_detect_mode, 'yt_mem': yt_mem, 
-			'pm_mem': pm_mem, 'sp_mem': sp_mem, 'sp_as': sp_as, 'yt_as': yt_as, 'pm_as': pm_as},
-			function(){
-			$("#response_msg").text("Options saved - Reload any pages with Lyrical running for changes to take effect");
-			setTimeout(function(){
-				$("#response_msg").text("");
-			}, 2000);
-		});
+		chrome.storage.local.set(
+			OPTS,
+			() => {
+				showResMsg("Options saved - Reload any pages with Lyrical running for changes to take effect");
+			}
+		);
 		e.preventDefault();
 		e.stopPropagation();
 	}
 
 	// Restore default options
 	function restore_defaults(e){
-		chrome.storage.local.set({'run_on_gp': true, 'run_on_yt': true, 'run_on_sp': true, 'autoscroll': false, 
-			'run_all': false, 'yt_detect_mode': true, 'yt_mem': true, 'pm_mem': true, 'sp_mem': true,
-			'yt_as': false, 'pm_as': false, 'sp_as': false}, function(){
+		chrome.storage.local.set(OPTS, () => {
 				pull_options();
-				$("#response_msg").text("Options restored to defaults");
-				setTimeout(function(){
-					$("#response_msg").text("");
-				}, 1500);
+				showResMsg("Options restored to defaults");
 		});
 		e.preventDefault();
 		e.stopPropagation();
@@ -87,6 +89,13 @@ $(function(){
 		return false;
 	})
 	$(".more-text").width($("#yt_options").width());
+
+	$('.clear-cache').on('click', e => {
+		e.preventDefault();
+		chrome.runtime.sendMessage({message: "clear-cache"});
+		showResMsg("Lyrics cache cleared");
+
+	})
 
 	pull_options();
 });
